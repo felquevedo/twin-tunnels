05c7f51e6bac1c5bb639ed84e98122244b6b59a0
Felipe Quevedo <motta.quevedo@ufrgs.br>
2023-09-29 17:57:52 -0300

    Add Usermat compile files

--- USERMAT/USERMAT_COMPILADA/usermat3d_epvp__genmod.f90
+++ USERMAT/USERMAT_COMPILADA/usermat3d_epvp__genmod.f90
@@ -1,4 +1,4 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Mon Sep 25 09:49:25 2023
+        !COMPILER-GENERATED INTERFACE MODULE: Fri Sep 29 17:56:22 2023
         ! This source file is for reference only and may not completely
         ! represent the generated interface used by the compiler.
         MODULE USERMAT3D_EPVP__genmod
