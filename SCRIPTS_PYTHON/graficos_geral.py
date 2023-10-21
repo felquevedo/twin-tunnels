2dd9f673e5d6b59abe21ac267f0be6cf205c3388
Felipe Quevedo <motta.quevedo@ufrgs.br>
2023-10-21 10:01:27 -0300

    Add ScrIpt Python
    Add script in python to plot convergence profiles

---
+++ SCRIPTS_PYTHON/graficos_geral.py
@@ -0,0 +1,237 @@
+# -*- coding: utf-8 -*-
+""" ********************************************
+SCRIPT PARA PLOTAGEM DA CURVA DE CONVERGÊNCIA
+DAS ANÁLISES EM AXISSIMETRIA
+Versão: 2023.1
+Situação : Teste (11/07/2020) 
+******************************************** """ 
+
+# Importar bibliotecas
+import pandas as pd                   # importa o pacote para manmipular tabelas 
+import matplotlib.pyplot as plt       # importa o módulo para plotagem
+import numpy as np                    # importa o pacote para operar arrays
+from matplotlib import rc             # importa a função com os parâmetros de configuração da plotagem
+from scipy.interpolate import interp1d
+from scipy.signal import savgol_filter
+#
+#
+def graficar(arquivo,                   # nome do arquivo de leitura
+             titulo,                    # titulo do gráfico
+             eixox,eixoy,               # nome dos eixos x e y
+             xmin,xmax,                 # intervalo eixo x
+             ymin,ymax,                 # intervalo eixo y
+             ncoluna,                   # numero da coluna (dados em y)
+             lblcoluna,                 # legenda da coluna
+             cor,tamanho,ordem,alpha,   # formatacao
+             filterx1,filterx2,wl,poly, # parametros do filtro de suavização
+             figura):
+    
+    # Lendo arquivo de dados      
+    data = pd.read_csv(arquivo,delim_whitespace= True).values
+
+    # Definindo as dimensões do layout da figura
+    plt.figure(figura,figsize = (9,5))
+
+    # Eixo x
+    x = -(data[:,1]-max(data[:,1]))
+    #x = data[:,1]
+
+    # dados do eixo y
+    y = data[:,ncoluna]
+    #y = savgol_filter(data[:,ncoluna],20,10)
+    
+    for i in range(1,3):
+       y[filterx1:filterx2] = savgol_filter(data[filterx1:filterx2,ncoluna],wl,poly, mode = 'interp')
+        
+    # Plotando os dados
+    plt.plot(x,y,color = cor,zorder = ordem, lw = tamanho, alpha = alpha, label = lblcoluna)
+
+    # Formatando os eixos
+    #plt.axvline(x0,color = 'k', label = lblx0, linestyle = '--')
+    plt.ylim([ymin, ymax])
+    plt.xlim([xmin, xmax])
+    plt.ylabel(eixoy)
+    plt.xlabel(eixox)
+    plt.legend()
+    plt.grid(True) 
+    plt.title(titulo) 
+    #plt.autoscale(axis='y')
+    
+    # Formatando a legenda
+    plt.legend(
+        loc = 'center',
+        shadow=False,
+        framealpha = 0.8,
+        ncol = 4,
+        columnspacing = 0.5,
+        bbox_to_anchor=(0.5, -0.2))
+    
+    # Salvando em arquivo    
+    plt.savefig('profile'+ str(figura) + '.pdf')
+
+# Formatação do gráfico
+titulo      = 'Convergence Profiles'
+eixox       = r'$y/R_e$'  
+eixoy       = r'$U=-u(R_e,90^\circ)/R_{e}$ [\%]'
+ymin        = 0
+ymax        = 1.5
+xmin        = 5
+xmax        = 35
+
+# parametros para o filtro de suavização
+filterx1    = 35
+filterx2    = 90
+wl          = 30
+poly        = 10
+
+
+figura      = 1
+arquivo     = 'EP_CRE_CG_D1_4RE_3D.txt'
+ncoluna     = 81
+lblcoluna   = 'd1 = 4Re'
+cor         = 'b'
+tamanho     = 3
+ordem       = 1
+alpha       = 1
+graficar(arquivo,titulo,eixox,eixoy,
+         xmin,xmax,ymin,ymax,
+         ncoluna,lblcoluna,
+         cor,tamanho,ordem,alpha,
+         filterx1,filterx2,wl,poly,
+         figura)
+
+figura      = 1
+arquivo     = 'EPVP_CRVE_CG_D1_4RE_3D.txt'
+ncoluna     = 111
+lblcoluna   = 'EPVP - d1 = 4Re'
+cor         = 'pink'
+tamanho     = 3
+ordem       = 1
+alpha       = 1
+graficar(arquivo,titulo,eixox,eixoy,
+         xmin,xmax,ymin,ymax,
+         ncoluna,lblcoluna,
+         cor,tamanho,ordem,alpha,
+         filterx1,filterx2,wl,poly,
+         figura)
+
+figura      = 1
+arquivo     = 'EP_CRE_CG_D1_8RE_3D.txt'
+ncoluna     = 90
+lblcoluna   = 'd1 = 8Re'
+cor         = 'g'
+tamanho     = 3
+ordem       = 1
+alpha       = 1
+graficar(arquivo,titulo,eixox,eixoy,
+          xmin,xmax,ymin,ymax,
+          ncoluna,lblcoluna,
+          cor,tamanho,ordem,alpha,
+          filterx1,filterx2,wl,poly,
+          figura)
+
+figura      = 1
+arquivo     = 'EP_CRE_CG_D1_16RE_3D.txt'
+ncoluna     = 107
+lblcoluna   = 'd1 = 16Re'
+cor         = 'r'
+tamanho     = 3
+ordem       = 1
+alpha       = 1
+graficar(arquivo,titulo,eixox,eixoy,
+          xmin,xmax,ymin,ymax,
+          ncoluna,lblcoluna,
+          cor,tamanho,ordem,alpha,
+          filterx1,filterx2,wl,poly,
+          figura)
+
+figura      = 1
+arquivo     = 'EP-CRE-SG-D1-INF-AXI.txt'
+ncoluna     = 79
+lblcoluna   = 'AXI'
+cor         = 'gray'
+tamanho     = 3
+ordem       = 1
+alpha       = 1
+graficar(arquivo,titulo,eixox,eixoy,
+          xmin,xmax,ymin,ymax,
+          ncoluna,lblcoluna,
+          cor,tamanho,ordem,alpha,
+          filterx1,filterx2,wl,poly,
+          figura)
+
+# Colocar uma linha vertical
+x0          = 80*1/3
+lblx0       = 'Face'
+plt.axvline(x0,color = 'k', label = lblx0, linestyle = '--')
+
+
+#GALERIA
+
+
+# Formatação do gráfico
+titulo      = 'Gallery Convergence Profiles'
+eixox       = r'$y/R_{e1}$'  
+eixoy       = r'$U{1}=-u{1}(R_e{1},90^\circ)/R_{e1}$ [\%]'
+ymin        = 0.75
+ymax        = 1.9
+xmin        = 0
+xmax        = 12
+
+# parametros para o filtro de suavização
+filterx1    = 0
+filterx2    = 80
+wl          = 20
+poly        = 6
+
+
+figura      = 2
+arquivo     = 'EPVP_CRVE_CG_D1_16RE_3D_GALERIA.txt'
+ncoluna     = 79
+lblcoluna   = 'd1 = 16Re'
+cor         = 'b'
+tamanho     = 3
+ordem       = 1
+alpha       = 1
+graficar(arquivo,titulo,eixox,eixoy,
+          xmin,xmax,ymin,ymax,
+          ncoluna,lblcoluna,
+          cor,tamanho,ordem,alpha,
+          filterx1,filterx2,wl,poly,
+          figura)
+
+figura      = 2
+arquivo     = 'EPVP_CRVE_CG_D1_8RE_3D_GALERIA.txt'
+ncoluna     = 79
+lblcoluna   = 'd1 = 8Re'
+cor         = 'r'
+tamanho     = 3
+ordem       = 1
+alpha       = 1
+graficar(arquivo,titulo,eixox,eixoy,
+          xmin,xmax,ymin,ymax,
+          ncoluna,lblcoluna,
+          cor,tamanho,ordem,alpha,
+          filterx1,filterx2,wl,poly,
+          figura)
+
+# parametros para o filtro de suavização
+filterx1    = 0
+filterx2    = 80
+wl          = 10
+poly        = 6
+
+figura      = 2
+arquivo     = 'EPVP_CRVE_CG_D1_4RE_3D_GALERIA.txt'
+ncoluna     = 79
+lblcoluna   = 'd1 = 4Re'
+cor         = 'r'
+tamanho     = 3
+ordem       = 1
+alpha       = 1
+graficar(arquivo,titulo,eixox,eixoy,
+          xmin,xmax,ymin,ymax,
+          ncoluna,lblcoluna,
+          cor,tamanho,ordem,alpha,
+          filterx1,filterx2,wl,poly,
+          figura)
